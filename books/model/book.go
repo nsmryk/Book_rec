ccf63ff9072e3b339e152a35b7c315e24bedb279
yuki nishimura <yukinishimura@yukinoMacBook-Air.local>
2021-01-15 22:48:08 +0900

    Modify Add

--- books/model/book.go
+++ books/model/book.go
@@ -1,7 +1,7 @@
 package model
 
 type Book struct {
-	Id    int64  `xorm:"pk" form:"id" json:"id"`
+	Id    int64  `xorm:"pk autoincr" form:"id" json:"id"`
 	Title string `form:"title" json:"title"`
 	Score int64  `form:"score" json:"score"`
 	Memo string `form:"memo" json:"memo"`
